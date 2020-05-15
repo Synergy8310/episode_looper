8eead97ba1f631e640c1aba42b7ee0ed4504f235
Synergy8310 <jacob.boudreau8@gmail.com>
2020-05-15 15:07:18 -0400

    Still need to find efficient video format.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import subprocess
-episode = subprocess.call(["ls | shuf -n 1"])
-subprocess.call(["vlc episode/" + episode])
+import os, random
+
+#Selects a random file from the episodes folder
+path = os.getcwd() + '/episodes'
+files = os.listdir(path)
+index = random.randrange(0, len(files))
+episode = path + '/' + (files[index])
+
+#opens the random file with omxplayer
+os.system('./omxplayer -p -o hdmi ' + path)
